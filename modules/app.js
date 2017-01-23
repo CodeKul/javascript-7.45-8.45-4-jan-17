717946c5a9035dc14dabb891a58bc593b0054332
aniruddha <aniruddha.kudalkar@gmail.com>
2017-01-23 21:17:18 +0530

    dom manipulation

--- modules/app.js
+++ modules/app.js
@@ -1,13 +1,23 @@
-var walletModule = ((wsWallet) => {
+var walletModule = ( wsWallet => {
     'use strict';
 
     //return () => {}; // function
     return {
         pay: (amt, walletId) => wsWallet.webPay(amt, walletId),
         topUp: (amt, walletId) => wsWallet.webTopUp(amt, walletId),
-        register: (userName) => wsWallet.webRegister(userName)
+        register: (userName) => wsWallet.webRegister(userName),
+        users : (callBack) => wsWallet.users(callBack)
     }; // obj literal
 })(walletService);
 
-
-walletModule.register('c++');
+document.getElementById('btnRegister').onclick = () => walletModule.register(document.getElementById('textUser').value);
+document.getElementById('btnUsers').onclick = () => {
+    walletModule.users( (users) => {
+        let obj = JSON.parse(users);
+        for(val in obj) {
+            let div = document.createElement('div');
+            div.innerHTML = "<hr><h3><a>"+ obj[val]['walletUser'] +'</a></h3>';
+            document.getElementById('divUsers').appendChild(div);
+        }
+    });
+};
--- modules/wallet.js
+++ modules/wallet.js
@@ -15,8 +15,18 @@ var walletService = (() => {
                 if (this.readyState == 4 && this.status == 200) {
                 }
             };
-            xhttp.open("POST", "https://angular-demo-d9927.firebaseio.com/walletUser.json", true);
+            xhttp.open("POST", "https://digital-shelter-153912.firebaseio.com/walletUser.json", true);
             xhttp.send(JSON.stringify({ walletUser: userName }));
+        },
+        users : (callBack) => {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    callBack(xhttp.responseText);
+                }
+            };
+            xhttp.open("GET", "https://digital-shelter-153912.firebaseio.com/walletUser.json", true);
+            xhttp.send();
         }
     };
 })();
